a45e7bde54abf78678cf9107f1e53cf5d0e354c5
JeroenoBoy <Jeroen@vandegeest.eu>
2021-10-15 20:17:58 +0200

    Build

--- dist/index.js
+++ dist/index.js
@@ -5568,18 +5568,40 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(186);
 const fetch = __nccwpck_require__(467);
 
-if (!core.getInput('PTE_PANEL_URL')) {
-    core.error('No pterodactyl URL was supplied');
-    process.exit(1);
+let token    = core.getInput('PTE_BEARER_TOKEN');
+let panelUrl = core.getInput('PTE_PANEL_URL');
+let panelId  = core.getInput('PTE_PANEL_ID');
+
+//  Checking if any variable is not set
+
+switch('') {
+    case panelUrl:
+        core.error('No pterodactyl URL was supplied');
+        process.exit(1);
+        break;
+    
+    case token:
+        core.error('No pterodactyl bearer token was supplied');
+        process.exit(1);
+        break;
+
+    case panelId:
+        core.error('No pterodactyl panel ID was supplied');
+        process.exit(1);
+        break;
 }
-if (!core.getInput('PTE_BEARER_TOKEN')) {
-    core.error('No pterodactyl bearer token was supplied');
-    process.exit(1);
+
+//  Parsing panel URL
+
+let url;
+
+try {
+    url = new URL(`${panelUrl}/api/client/servers/${encodeURIComponent(panelId)}/power`);
+} catch(e) {
+    core.error('Misformed URL');
 }
-if (!core.getInput('PTE_PANEL_ID')) {
-    core.error('No pterodactyl panel ID was supplied');
-    process.exit(1);
-}
+
+//  Preparing 
 
 const headers = {
     'Authorization': `Bearer ${core.getInput('PTE_BEARER_TOKEN')}`,
@@ -5590,15 +5612,14 @@ const body = {
     "signal": "restart"
 }
 
-const url = `${core.getInput('PTE_PANEL_URL')}/api/client/servers/${core.getInput('PTE_PANEL_ID')}/power`;
-fetch(url, { method: 'POST', headers, body: JSON.stringify(body) })
-.then(async (res) => {
-    if (res.ok) process.exit(0)
-    else {
-        core.error(`Invalid response! GOT ${res.status}. Message: ${await res.text()}`);
-        process.exit(1);
-    }
-})
+fetch(url.toString(), { method: 'POST', headers, body: JSON.stringify(body) })
+    .then(async (res) => {
+        if (res.ok) process.exit(0)
+        else {
+            core.error(`Invalid response! GOT ${res.status}. Message: ${await res.text()}`);
+            process.exit(1);
+        }
+    })
 
 
 })();
